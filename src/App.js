0bf11029d85896b6f059b6b13d8d48d6b388bd5d
Dariusz Okoński <darek200180@gmail.com>
2024-07-17 18:33:52 +0200

    add Filter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import Cards from './components/Cards/Cards';
+import cats from './mocks/cats.json';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Cards cats={cats}></Cards>
+    </div>
+  );
 }
 
 export default App;
