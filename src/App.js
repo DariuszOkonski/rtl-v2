d611c4ba6b2d9203ee2eb517af0f4d09fcc0ee79
Dariusz Okoński <darek200180@gmail.com>
2024-07-17 18:38:36 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Cards from './components/Cards/Cards';
+import Filter from './components/Filter/Filter';
 import cats from './mocks/cats.json';
 
 function App() {
   return (
     <div className='App'>
-      <Cards cats={cats}></Cards>
+      <div className='app-container'>
+        <Filter />
+        <Cards cats={cats}></Cards>
+      </div>
     </div>
   );
 }
