14b9664d39697f749b28013bf49d2242241d0581
Dariusz Okoński <darek200180@gmail.com>
2024-07-17 18:46:32 +0200

    update port

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const { cats } = require("./data");
-const cors = require("cors");
+const express = require('express');
+const { cats } = require('./data');
+const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
 
-app.get("/cats", async (req, res) => {
+app.get('/cats', async (req, res) => {
   return res.json(cats);
 });
 
 app.listen(4000, () => {
-  console.log("listening");
+  console.log('listening at port 4000...');
 });
