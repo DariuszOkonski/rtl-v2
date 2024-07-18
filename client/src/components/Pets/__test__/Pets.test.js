94a26593cead89693409f6295fb3da3a9b5048b1
Dariusz Okoński <darek200180@gmail.com>
2024-07-18 17:12:38 +0200

    add test for Pets

---
+++ client/src/components/Pets/__test__/Pets.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import Pets from '../Pets';
+
+describe('Pets', () => {
+  test('should render the correct amount of cards', async () => {
+    render(<Pets />);
+    const cards = await screen.findAllByRole('article');
+
+    expect(cards.length).toBe(5);
+  });
+});
