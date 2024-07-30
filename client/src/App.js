e27131d994ff179ef2a71c34898e8c6b5cad3dc1
Dariusz Okoński <darek200180@gmail.com>
2024-07-30 19:09:06 +0200

    working on Filter

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Cards from './components/Cards/Cards';
 import cats from './mocks/cats.json';
+import Filter from './components/Filter/Filter';
 
 function App() {
   return (
     <div className='App'>
+      <Filter />
       <Cards cats={cats} />
     </div>
   );
--- client/src/components/Filter/__test__/Filter.test.js
+++ client/src/components/Filter/__test__/Filter.test.js
@@ -2,9 +2,10 @@ import { render, screen } from '@testing-library/react';
 import Filter from '../Filter';
 import userEvent from '@testing-library/user-event';
 
-describe.skip('Filter', () => {
+describe('Filter', () => {
   test('should be able to change value of favoured select', async () => {
     render(<Filter />);
+
     const select = screen.getByLabelText(/favourite/i);
     expect(select.value).toBe('any');
 
@@ -14,18 +15,20 @@ describe.skip('Filter', () => {
     await userEvent.selectOptions(select, 'not favoured');
     expect(select.value).toBe('not favoured');
   });
-
-  test('should be able to change value of gender select', async () => {
-    render(<Filter />);
-
-    const select = screen.getByLabelText(/gender/i);
-
-    expect(select.value).toBe('any');
-
-    await userEvent.selectOptions(select, 'male');
-    expect(select.value).toBe('male');
-
-    await userEvent.selectOptions(select, 'female');
-    expect(select.value).toBe('female');
-  });
 });
+
+// describe.skip('Filter', () => {
+//   test('should be able to change value of gender select', async () => {
+//     render(<Filter />);
+
+//     const select = screen.getByLabelText(/gender/i);
+
+//     expect(select.value).toBe('any');
+
+//     await userEvent.selectOptions(select, 'male');
+//     expect(select.value).toBe('male');
+
+//     await userEvent.selectOptions(select, 'female');
+//     expect(select.value).toBe('female');
+//   });
+// });
