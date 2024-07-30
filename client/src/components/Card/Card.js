eb7a70da1df0d62fa41785a75c0907f02440c2e0
Dariusz Okoński <darek200180@gmail.com>
2024-07-30 18:21:53 +0200

    add tests for Card

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -4,10 +4,23 @@ import heartOutlined from '../../svgs/heartOutlined.svg';
 import './Card.css';
 
 const Card = ({ name, phone, email, image, favoured }) => {
+  const [isFavoured, setIsFavoured] = useState(favoured);
+
+  const toggleFavoured = () => {
+    setIsFavoured(!isFavoured);
+  };
+
   return (
     <div className='card'>
       <div className='card-header'>
         <img src={image.url} alt={image.alt} className='card-img' />
+        <button className='heart' onClick={toggleFavoured}>
+          {isFavoured ? (
+            <img src={heartFilled} alt='filled heart' />
+          ) : (
+            <img src={heartOutlined} alt='outlined heart' />
+          )}
+        </button>
       </div>
       <div className='card-content'>
         <h3>{name}</h3>
@@ -19,33 +32,3 @@ const Card = ({ name, phone, email, image, favoured }) => {
 };
 
 export default Card;
-
-// const Card = ({ name, phone, email, image, favoured }) => {
-//   const [isFavoured, setIsFavoured] = useState(favoured);
-
-//   const toggleFavoured = () => {
-//     setIsFavoured(!isFavoured);
-//   };
-
-//   return (
-//     <article className='card'>
-//       <div className='card-header'>
-//         <img src={image.url} alt={image.alt} className='card-img' />
-//         <button className='heart' onClick={toggleFavoured}>
-//           {isFavoured ? (
-//             <img src={heartFilled} alt='filled heart' />
-//           ) : (
-//             <img src={heartOutlined} alt='outlined heart' />
-//           )}
-//         </button>
-//       </div>
-//       <div className='card-content'>
-//         <h3>{name}</h3>
-//         <p>{phone}</p>
-//         <p>{email}</p>
-//       </div>
-//     </article>
-//   );
-// };
-
-// export default Card;
